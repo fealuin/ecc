7d4d750dbfba37abef7f2041177ef8c6a121155d
Juan Giglio <Juan@Juans-MBP.iptv\.microsoft\.com>
2015-06-18 07:24:37 -0300

    Se mejora validacion de punto generador

--- ecc.py
+++ ecc.py
@@ -8,7 +8,7 @@ def validaCoeficientes(a,b):
 def sumaPuntos(p,q,P,a): #Suma puntos de la curva eliptica
 
 	if(p==q):
-		for i in range (1,1000):
+		for i in range (1,2000):
 			x=(P*i+1.0)/(2*p[1])
 			if x-int(x) == 0:
 				s=(3*pow(p[0],2)+a)*int(x)%P
@@ -18,6 +18,9 @@ def sumaPuntos(p,q,P,a): #Suma puntos de la curva eliptica
 		print "%s + %s = %s"%(str(p),str(q),tuple((rx%P,ry%P)))
 		return tuple((rx%P,ry%P))
 	else:
+		if(p[0]==q[0]):
+			print "IGUALESS %s + %s = %s"%(str(p),str(q),str(q))
+			return q
 		#print "sumando puntos distintos %s %s"%(str(p),str(q))
 		for i in range (1,1000):
 			x=(P*i+1.0)/(p[0]-q[0])
@@ -34,10 +37,16 @@ def restaPuntos(p,q,P,a): #Suma el inverso aditivo de q a p
 	return sumaPuntos(p,tuple((q[0],-q[1])),P,a)
 
 def multiplicaPunto(n,p,P,a): #multiplica puntos
-	if n==1:
-		return p
-	else:
-		return sumaPuntos(multiplicaPunto(n-1,p,P,a),p,P,a)
+	for i in range(n):
+		if i==0:
+			a=p
+		a=sumaPuntos(a,p,P,a)
+	return a
+
+	#if n==1:
+	#	return p
+	#else:
+	#	return sumaPuntos(multiplicaPunto(n-1,p,P,a),p,P,a)
 
 def puntoEnCurva(p,P,a,b): #Retorna True si el punto pertenece a la curva
 	z=(pow(p[0],3)+a*p[0]+b)%P
@@ -60,18 +69,20 @@ def esPrimo(n):
 
 def validaGenerador(g,P,a,b):
 	if puntoEnCurva(g,P,a,b):
-		for i in range(2,1000):
-			if multiplicaPunto(i,g,P,a)==tuple((g[0],-g[1])):
-				print i
+		i=2
+		punto=g
+		while True:
+			punto=sumaPuntos(punto,g,P,a)
+			i=i+1
+			if punto[0]==g[0]: 
 				if esPrimo(i):
+					print i
 					return True
-				else:
-					return False
 	return False
 
-#print validaGenerador((22, 21),23,3,8)
+print validaGenerador((6, 9),23,3,8)
 #print puntoEnCurva((1,22),23,3,8)
 #puntosEnCurva(23,3,8)
-#print sumaPuntos((5,2),(3,7),11,1)
+#print sumaPuntos((2,7),(2,7),11,1)
 #print multiplicaPunto(7,(2,7),11,1)
 #print sumaPuntos((10,-2),(8,3),11)
